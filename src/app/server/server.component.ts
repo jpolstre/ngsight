5e32afff5c51f8947201cab33dc23c88b45abc2d
jpolstre <jpolstre@gmail.com>
2020-10-26 17:45:04 -0400

    Update server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,12 +8,32 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  /*es como props deen Vue */
-  @Input() serverInput:Server;
+  /*es como props en Vue, ademas con @Input manda al padre todo lo que se modifica en el objeto(propiedad)*/
+  @Input() serverInput: Server;
+
+  public color: string;
+  public buttonText: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline)
+  }
+
+  private setServerStatus(isOnline: boolean) {
+    this.serverInput.isOnline = isOnline
+    if (isOnline) {
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.color = '#ff6b6b';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(isOnlineStatus: boolean) {
+    console.log(this.serverInput.name, ':', isOnlineStatus);
+    this.setServerStatus(!isOnlineStatus);
   }
 
 }
