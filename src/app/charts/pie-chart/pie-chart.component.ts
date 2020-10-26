4bb310952c22de6a0e441786f55a6d12edfdf11b
jpolstre <jpolstre@gmail.com>
2020-10-26 14:39:37 -0400

    Add pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const SAMPLE_PIECHART_DATA: any[] = [
+  { data: [350, 450, 120] },
+];
+
+
+const SAMPLE_PIECHART_LABELS: string[] = ['xyz Logistic', 'Main St Bakery', 'Acme Hosting'];
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -8,6 +15,23 @@ import { Component, OnInit } from '@angular/core';
 export class PieChartComponent implements OnInit {
 
   constructor() { }
+  public pieChartData: any[] = SAMPLE_PIECHART_DATA;
+  public pieChartLabels: string[] = SAMPLE_PIECHART_LABELS;
+  public pieChartType = 'doughnut';
+  public pieChartLegend = true;
+  public colors = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ]
+
+  public pieChartOptions: any = {
+    scaleShadowVerticalLines: false,
+    responsive: true,
+
+
+  }
 
   ngOnInit(): void {
   }
