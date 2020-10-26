dce7bff47846bb5e31f8ec5bf5360dd14a6b8c6d
jpolstre <jpolstre@gmail.com>
2020-10-26 13:06:46 -0400

    Add Componentes Charts and Create bar chart, directory structure, update styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { SectionSalesComponent } from './sections/section-sales/section-sales.co
     SidebarComponent,
     SectionHealthComponent,
     SectionOrdersComponent,
-    SectionSalesComponent
+    SectionSalesComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
