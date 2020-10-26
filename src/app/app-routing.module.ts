03d4cd83d906007a98791e0331107b85da445ede
jpolstre <jpolstre@gmail.com>
2020-10-26 12:21:42 -0400

    Add sections components and config routes for those sections.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sales', component: SectionSalesComponent },
+  { path: 'orders', component: SectionOrdersComponent },
+  { path: 'health', component: SectionHealthComponent },
+
+  { path: '', redirectTo: '/sales', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
